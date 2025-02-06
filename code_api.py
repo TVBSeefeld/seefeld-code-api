1e9d33168de1e21664e4dd4658ae317c6fa195e6
TVBSeefeld <christian.lackner@seefeld.com>
2025-02-06 12:17:25 +0100

    Update code_api.py

--- code_api.py
+++ code_api.py
@@ -29,6 +29,8 @@ def get_code():
             return jsonify({"success": True, "code": item["code"]})
     
     return jsonify({"success": False, "message": "Keine Codes mehr verfügbar."})
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Standard-Port 10000 setzen
+    app.run(host="0.0.0.0", port=port)
+
